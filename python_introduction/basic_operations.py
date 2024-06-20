60b907d6a54038e3770e8eac0d4bf8ae846d3e25
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-06-20 18:08:22 +0300

    Added basic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number1 = int(10)
-number2 = int(5)
-sum = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+number1 = 10
+number2 = 5 
+sum = int(number1) + (number2)
+subtraction = int(number1) - int(number2)
+multiplication = int(number1) * int(number2)
 print("Addition of", number1, "and", number2, "is", sum)
 print("Subtraction of", number1, "and", number2, "is", subtraction)
 print("Multiplication of", number1, "and", number2, "is", multiplication)
