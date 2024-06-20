0aa690b0bf0e60c73fb37058efa6ec76334fe24c
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-06-20 21:25:39 +0300

    simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,8 @@
-principal = 1000
-rate = 0.05
-time = 3
-interest = float(principal) * float(rate) * float(time)
-print("The simple interest is: ", interest)
+P = 1000
+R = 0.05
+T = 3
+principal = P
+rate = R
+time = T
+I = float(P) * float(R) * float(T)
+print("The simple interest is: ", I)
